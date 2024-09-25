61ecb949c5954aae4ce4028f57e27e0ed3510cfa
Aril <139083873+zendee-bot@users.noreply.github.com>
2024-09-25 23:48:30 +0700

    Update fungsi.py

--- CSV READ/fungsi.py
+++ CSV READ/fungsi.py
@@ -35,23 +35,3 @@ def parse_dictionary(data):
         dict_result[key.strip()] = value.strip()
     return dict_result
 
-def main():
-    while True:
-        nama_file = input("Masukkan nama file txt/csv: ")
-        isi_file = baca_file(nama_file)
-        if isi_file is not None:
-            # Menangani data sebagai array
-            data_array = parse_array(isi_file)
-            print("\nData yang dibaca adalah Array:")
-            print(data_array)
-        else:
-            print(f"File '{nama_file}' tidak ditemukan. Coba lagi.\n")
-        
-        # Tanyakan apakah ingin memasukkan file lain
-        pilihan = input("\nApakah Anda ingin memasukkan file lain? (y/n): ").lower()
-        if pilihan != 'y':
-            print("Terima kasih telah menggunakan aplikasi!")
-            break
-
-if __name__ == "__main__":
-    main()
