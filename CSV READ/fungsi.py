1985be5cd9334cbd34d31892ef93c6555300880c
Aril <139083873+zendee-bot@users.noreply.github.com>
2024-09-25 23:28:23 +0700

    Update fungsi.py

--- CSV READ/fungsi.py
+++ CSV READ/fungsi.py
@@ -15,6 +15,26 @@ def parse_array(data):
         array = [item.strip() for item in data.splitlines() if item.strip()]
     return array
 
+def parse_dictionary(data):
+    dict_result = {}
+    
+    if '=>' in data:
+        pairs = data.split(',')
+    elif ':' in data:
+        pairs = data.split(';')
+    else:
+        pairs = []
+    
+    for pair in pairs:
+        if '=>' in pair:
+            key, value = pair.split('=>')
+        elif ':' in pair:
+            key, value = pair.split(':')
+        else:
+            continue
+        dict_result[key.strip()] = value.strip()
+    return dict_result
+
 def main():
     while True:
         nama_file = input("Masukkan nama file txt/csv: ")
