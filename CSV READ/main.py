1e0fe839ab2d02dcfec32ae9673a3109a1485aa6
Syeikh Kafila Santery <syehsantery@gmail.com>
2024-10-02 21:20:30 +0700

    tambah fungsi menambahkan data

--- CSV READ/main.py
+++ CSV READ/main.py
@@ -1,5 +1,5 @@
 from fungsi import baca_file, parse_array, parse_dictionary, hapus_data
-#saya aslinya tiga
+
 
 def main():
     # Muat data dari file saat aplikasi dimulai
@@ -8,12 +8,14 @@ def main():
         print("1. Lihat Data")
         print("2. Hapus Data")
         print("3. Tutup Aplikasi")
-        pilihan = input("Pilih opsi (1/2/3): ")
+        print("4. Tambah Data")
+        pilihan = input("Pilih opsi (1/2/3/4): ")
 
         if pilihan == '1':
             print("1. Lihat Data Warna")
             print("2. Lihat Data Merek")
             print("3. Lihat Data Mobil")
+            
             pilihan = input("Pilihan : ")
             if pilihan == '1':
                 isi = baca_file(nama_file="data_warna.txt")
@@ -29,7 +31,8 @@ def main():
                 isi = baca_file(nama_file="data_mobil.txt")
                 data_dict = parse_dictionary(isi)
                 print("\nData yang dibaca adalah Dictionary:")
-                print(data_dict)   
+                print(data_dict)
+               
             
         elif pilihan == '2':
             print("Pilih File yang ingin di hapus : ")
